61de17467cfc345fd494f86fd8c0cf4ee7604144
omenemo <nemo7brigatti@hotmail.de>
2022-03-14 22:38:40 +0100

    path_loading&responsiveness

--- Skripts/OTF_Points_Edit_2022_03_14_17_30_18/points.js
+++ Skripts/OTF_Points_Edit_2022_03_14_17_30_18/points.js
@@ -3,33 +3,40 @@ let font; // opentype.js font object
 let fSize = "100"; // default font size
 let tPos = { x: 0, y: 0 };
 
-let msg = "hallo";
+let msg = "Meta";
 
 let path; //store path data
+let ctx;
+let advance;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0);
-  stroke(255);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  stroke(3);
   fill(3);
-  strokeWeight(2);
+  strokeWeight(10);
+  
   textSize(fSize);
   textAlign(CENTER);
-  tPos.x = windowWidth / 2;
-  tPos.y = windowHeight / 2;
-  fSize = (1.5 * windowWidth) / msg.length; //more dynamic fit
-
+  
   openTypeLoad();
+
+  fSize = 0.75 * windowHeight / msg.length; //more dynamic fit
+  tPos.y = windowHeight / 2 + fSize / 3;
+  tPos.x = (windowWidth/2) - (advance/2);
 
   if (!font) return;
   noFill();
   stroke(3);
   stroke(255, 0, 255);
   
-  //font.drawPoints(canvas, msg, tPos.x, tPos.y, fSize, true)
+  //font.draw(ctx, msg, tPos.x, tPos.y, fSize, true)
+  //font.drawPoints(ctx, msg, tPos.x, tPos.y, fSize, true)
+  //font.getPath(msg, tPos.x + advance, tPos.y, fSize, true);
+  path.draw(ctx);
 
   //vectorDraw();
   //noLoop();
@@ -57,11 +64,10 @@ function openTypeLoad() {
     } else {
       font = f;
       console.log("font ready");
-      let x = tPos.x / 4;
-      let y = tPos.y;
-      path = font.getPath(msg, x, y, fSize);
+      path = font.getPath(msg, tPos.x, tPos.y, fSize);
       console.log(path.commands);
-      const ctx = document.getElementById('canvas').getContext('2d');
+      ctx = document.getElementById('defaultCanvas0').getContext('2d');
+      advance = font.getAdvanceWidth(msg, fSize, true);
     }
   });
 }
